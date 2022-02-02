bdcd645f6bd18c47688983cff4c24a061aadfda6
BrettBeck <62182056+BrettBeck@users.noreply.github.com>
2022-02-02 16:17:37 +0100

    ajout de l'interface pour l'objet "Point"

---
+++ sample.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+    export interface Point {
+        x: number;
+        y: number;
+        t: number;
+        isSelected : boolean;
+    }
+
+    function toString(){
+        return( <Text>{}</Text>) 
+    }
