bb3ba6b08ec3d4321486bdc64038749ce790e2ac
BrettBeck <62182056+BrettBeck@users.noreply.github.com>
2022-02-02 15:52:49 +0100

    affichage des points

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,39 +1,50 @@
 import React from 'react';
-import Svg, {Circle, Path, Polyline} from 'react-native-svg';
+import Svg, {Circle, Polyline} from 'react-native-svg';
+import {Point} from "./sample"
 import {
   Alert,
   View,
+  Text,
 } from 'react-native';
 
+const data = "" // JSON
+const json = JSON.parse(data)
 
+const getData = () => {
+  const points = json.ink.traceGroup.trace.toString().split(",")
+  let parsedPoints:Point[] = new Array()
+  points.forEach( (point: String) => {
+    const e :Point = { x: 0, y: 0, t: 0, isSelected : false};
+    e.x = parseFloat(point.split(" ")[0])
+    e.y = parseFloat(point.split(" ")[1])
+    parsedPoints.push(e) //le premier point va se retrouver à la fin de l'array
+  });
+  console.log(parsedPoints) 
+  return parsedPoints //slice pour retirer des éléments du tableau
+}
+
+const dispSVG = (parsedPoints: Point[]) => {
+    return(
+      <Svg width="900" height="900">
+        <Polyline
+          points={parsedPoints.map(({x, y}) => `${x+500},${y+200}`).join(' ')}
+          strokeWidth="2"
+          stroke="red"
+        />
+        <Circle onPress={() => Alert.alert('test')} cx="250" cy="100" r="10" />
+        <Circle onPress={() => Alert.alert('test')} cx="800" cy="650" r="10" />
+        <Circle onPress={() => Alert.alert('test')} cx="700" cy="250" r="10" />
+        <Circle onPress={() => Alert.alert('test')} cx="160" cy="770" r="10" />
+        <Circle onPress={() => Alert.alert('test')} cx="110" cy="300" r="10" />
+      </Svg>
+    );
+};
 
 const TestComponent = () => {
   return (
     <View>
-      <Svg width="900" height="900">
-        <Path
-          d="M250 100 L800 650 L700 250 L160 770 L110 300"
-          strokeWidth="2"
-          stroke="red"
-        />
-        <Circle onPress={() => Alert.alert('test')} cx="250" cy="100" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="800" cy="650" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="700" cy="250" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="160" cy="770" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="110" cy="300" r="10" />
-      </Svg>
-      <Svg width="900" height="900">
-        <Polyline
-          points="250,100 800,650 700,250 160,770 110,300"
-          strokeWidth="2"
-          stroke="red"
-        />
-        <Circle onPress={() => Alert.alert('test')} cx="250" cy="100" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="800" cy="650" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="700" cy="250" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="160" cy="770" r="10" />
-        <Circle onPress={() => Alert.alert('test')} cx="110" cy="300" r="10" />
-      </Svg>
+      <Text>{dispSVG(getData())}</Text>
+      
     </View>
   );
 };
