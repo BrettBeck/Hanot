e50d2ac401102c6692ce6a9897066a425ebaadee
Brett Becker <brett.becker@etudiant.univ-rennes1.fr>
2022-02-09 10:59:36 +0100

    ajout des points cliquables

--- sample.tsx
+++ sample.tsx
@@ -1,14 +1,6 @@
-import React from "react";
-import { Text } from "react-native";
-
-
-    export interface Point {
-        x: number;
-        y: number;
-        t: number;
-        isSelected : boolean;
-    }
-
-    function toString(){
-        return( <Text>{}</Text>) 
-    }
+export interface Point {
+  x: number;
+  y: number;
+  t: number;
+  isSelected: boolean;
+}
